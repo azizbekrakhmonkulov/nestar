e0eb08477e38641f623a5fb36c4a6f420d28365d
daniel <azizbekrakhmonkulov01@gmail.com>
2026-08-17 14:44:14 +0900

    feat: create member resolver & service model

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -9,12 +9,17 @@ import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
-    driver: ApolloDriver,
-    playgarund: true,
-    uploads: false,
-    autoSchemaFile: true,
-  }), ComponentsModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playgarund: true,
+      uploads: false,
+      autoSchemaFile: true,
+    }), 
+    ComponentsModule, 
+    DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
 })
