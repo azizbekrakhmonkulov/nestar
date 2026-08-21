31d5e67631ed140929faae34a6ef614a3f5b51a5
daniel <azizbekrakhmonkulov01@gmail.com>
2026-08-21 16:34:14 +0900

    feat: create getAllMembersByAdmin and updateMemberByAdmin GraphQL APIs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -63,4 +63,12 @@ export class MemberService {
     public async getMember(): Promise<string> {
         return 'GetMember executed!';
     }
+
+    public async getAllMembersByAdmin(): Promise<string> {
+        return 'GetAllMembersByAdmin executed!';
+    }
+
+    public async updateMemberByAdmin(): Promise<string> {
+        return 'UpdateMemberByAdmin executed!';
+    }
 }
