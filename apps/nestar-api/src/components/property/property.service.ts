1ac493bd35bd10e0762a8c71722cb9378c596c5c
daniel <azizbekrakhmonkulov01@gmail.com>
2026-08-31 19:01:08 +0900

    feat: Develop getAllpropertiesByAdmin GraphQL Api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -10,7 +10,7 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { ViewService } from '../view/view.service';
 import moment from 'moment';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
-import { AgentPropertiesInquiry, PropertiesInquiry } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry } from '../../libs/dto/property/property.input';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/confic';
 
 @Injectable()
@@ -195,6 +195,41 @@ export class PropertyService {
         const sort: T = {
             [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC,
         };
+
+        const result = await this.propertyModel
+            .aggregate([
+                { $match: match },
+                { $sort: sort },
+                {
+                    $facet: {
+                        list: [
+                            { $skip: (input.page - 1) * input.limit },
+                            { $limit: input.limit },
+                            lookupMember,
+                            { $unwind: '$memberData' },
+                        ],
+                        metaCounter: [{ $count: 'total' }],
+                    },
+                },
+            ])
+            .exec();
+
+        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        return result[0];
+    }
+
+    public async getAllPropertiesByAdmin(input: AllPropertiesInquiry): Promise<Properties> {
+        const { propertyStatus, propertyLocationList } = input.search;
+
+        const match: T = {};
+
+        const sort: T = {
+            [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC,
+        };
+
+        if (propertyStatus) match.propertyStatus = propertyStatus;
+        if (propertyLocationList) match.propertyLocation = { $in: propertyLocationList };
 
         const result = await this.propertyModel
             .aggregate([
