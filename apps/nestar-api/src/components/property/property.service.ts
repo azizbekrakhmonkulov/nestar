2cdd779b96975a1b1f7eb20ae17699f7954c9edf
daniel <azizbekrakhmonkulov01@gmail.com>
2026-08-31 18:56:54 +0900

    feat: develop getAgentsProperties business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -10,7 +10,7 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { ViewService } from '../view/view.service';
 import moment from 'moment';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
-import { PropertiesInquiry } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, PropertiesInquiry } from '../../libs/dto/property/property.input';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/confic';
 
 @Injectable()
@@ -180,5 +180,42 @@ export class PropertyService {
                 return { [ele]: true };
             });
         }
+    }
+
+    public async getAgentProperties(memberId: ObjectId, input: AgentPropertiesInquiry): Promise<Properties> {
+        const { propertyStatus } = input.search;
+
+        if (propertyStatus === PropertyStatus.DELETE) throw new BadRequestException(Message.NOT_ALLOWED_REQUEST);
+
+        const match: T = {
+            memberId: memberId,
+            propertyStatus: propertyStatus ?? { $ne: PropertyStatus.DELETE },
+        };
+
+        const sort: T = {
+            [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC,
+        };
+
+        const result = await this.propertyModel
+            .aggregate([
+                { $match: match },
+                { $sort: sort },
+                {
+                    $facet: {
+                        list: [
+                            { $skip: (input.page - 1) * input.limit },
+                            { $limit: input.limit },
+                            lookupMember,
+                            { $unwind: '$memberData' },
+                        ],
+                        metaCounter: [{ $count: 'total' }],
+                    },
+                },
+            ])
+            .exec();
+
+        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        return result[0];
     }
 }
