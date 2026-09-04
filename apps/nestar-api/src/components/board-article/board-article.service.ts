829afa068ffef2164d6c448b4882c494595f62c5
daniel <azizbekrakhmonkulov01@gmail.com>
2026-09-04 17:23:38 +0900

    feat: develop likeTargetArticle  GraphQl API

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -11,6 +11,9 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/confic';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { LikeService } from '../like/like.service';
 
 @Injectable()
 export class BoardArticleService {
@@ -19,6 +22,7 @@ export class BoardArticleService {
         @InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
         private readonly memberService: MemberService,
         private readonly viewService: ViewService,
+        private readonly likeService: LikeService,
     ) { }
 
     public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
@@ -95,6 +99,7 @@ export class BoardArticleService {
             match.memberId = shapeIntoMongoObjectId(input.search.memberId);
         }
 
+
         console.log('match:', match);
 
         const result = await this.boardArticleModel
@@ -119,6 +124,23 @@ export class BoardArticleService {
         if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
         return result[0];
+    }
+
+    public async likeTargetBoardArticle(memberId: ObjectId, likeRefId: ObjectId): Promise<BoardArticle> {
+        const target: BoardArticle | null = await this.boardArticleModel.findOne({ _id: likeRefId, articleStatus: BoardArticleStatus.ACTIVE }).exec();
+        if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        const input: LikeInput = {
+            memberId: memberId,
+            likeRefId: likeRefId,
+            likeGroup: LikeGroup.ARTICLE
+        };
+        // LIKE TOGGLE
+        const modifier: number = await this.likeService.toggleLike(input);
+        const result = await this.boardArticleStatsEditor({ _id: likeRefId, targetKey: "articleLikes", modifier: modifier });
+        if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+
+        return result;
     }
 
     public async boardArticleStatsEditor(input: StatisticModifier): Promise<BoardArticle> {
